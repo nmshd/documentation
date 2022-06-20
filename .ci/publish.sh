5c704aed85e1f65f8666c645091a4857f9c0fd3e
Julian König <33655937+jkoenig134@users.noreply.github.com>
2022-06-20 16:02:01 +0200

    Ci/consider release branches (#38)
    * chore: bump libs
    
    * ci: do publish manually
    
    * chore: actually use DEST
    
    * chore: remove next word

---
+++ .ci/publish.sh
@@ -0,0 +1,22 @@
+set -e
+
+CURRENT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+
+case "$CURRENT_BRANCH_NAME" in
+main)
+    DEST="."
+    ;;
+release/*)
+    # use the release version as folder (e.g. release/2.0.0 => versions/2.0.0)
+    DEST="${CURRENT_BRANCH_NAME/release/"./versions"}"
+    ;;
+*)
+    # exit if not on main or release/* branch
+    exit 1
+    ;;
+esac
+
+echo "Publishing to $DEST"
+
+git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
+npx gh-pages --dist _site --dest $DEST --branch gh-pages --user "github-actions-bot <support+actions@github.com>"
