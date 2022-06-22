6e1f1d4c7cf026be216d6e00fdf05a9bded97cea
Julian König <33655937+jkoenig134@users.noreply.github.com>
2022-06-22 08:33:58 +0200

    ci: do not overwrite PATH (#45)

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -8,14 +8,14 @@ main)
     ;;
 release/*)
     # use the release version as folder (e.g. release/2.0.0 => versions/2.0.0)
-    PATH="${CURRENT_BRANCH_NAME/release/"/versions"}"
-    DEST=".${PATH}"
+    DEST_PATH="${CURRENT_BRANCH_NAME/release/"/versions"}"
+    DEST=".${DEST_PATH}"
 
     # download and install yq
     wget https://github.com/mikefarah/yq/releases/download/v4.25.2/yq_linux_amd64 -O /usr/bin/yq && chmod +x /usr/bin/yq
 
     # update the baseurl in _config.yml
-    yq -i '.baseurl = "${PATH}"' _config.yml
+    yq -i '.baseurl = "${DEST_PATH}"' _config.yml
     ;;
 *)
     # exit if not on main or release/* branch
